943d53aa53a9106a4059123bb46d37293b2dba27
Fahrezi <luthfi0071@gmail.com>
2020-01-28 00:08:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+console.log(hamburger);
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navbar-desktop");
+  nav.getElementsByClassName.display = "flex";
+  console.log("click");
+});
